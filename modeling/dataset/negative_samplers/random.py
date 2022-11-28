b47941e273bf058d5b7bd4a198b2bbac7d791ca1
Noname Untitled <Noname-Untitled@yandex.ru>
2022-11-29 03:33:15 +0400

    Add negatives precomputation

--- modeling/dataset/negative_samplers/random.py
+++ modeling/dataset/negative_samplers/random.py
@@ -1,9 +1,41 @@
 from dataset.negative_samplers.base import BaseNegativeSampler
 
 import numpy as np
+from tqdm import trange
+from collections import defaultdict
 
 
 class RandomNegativeSampler(BaseNegativeSampler, config_name='random'):
+
+    def __init__(
+            self,
+            num_users,
+            num_items,
+            sample_size
+    ):
+        super().__init__(
+            num_users=num_users,
+            num_items=num_items,
+            sample_size=sample_size
+        )
+
+        print('Precomputing negatives...')
+        self._unseen_items = self._compute_negatives()
+        print('Precomputing negatives done!')
+
+    def _compute_negatives(self):
+        unseen_items = defaultdict(set)
+
+        for user_id in trange(self._num_users + 1):
+            for item_id in range(self._num_items + 1):
+                unseen_items[user_id].add(item_id)
+
+        for sample in self._dataset:
+            user_id = sample['item.ids'][0]
+            for item_id in sample['item.ids']:
+                unseen_items[user_id].remove(item_id)
+
+        return unseen_items
 
     @classmethod
     def create_from_config(cls, config, **kwargs):
@@ -13,15 +45,5 @@ class RandomNegativeSampler(BaseNegativeSampler, config_name='random'):
             sample_size=config['sample_size']
         )
 
-    def generate_negative_samples(self, items):
-        seen = set(items)
-        negative_samples = []
-
-        while len(negative_samples) < self._sample_size:
-            item = np.random.randint(1, self._num_items)
-
-            if item not in seen:
-                print('n', end='', flush=True)
-                negative_samples.append(item)
-
-        return negative_samples
+    def generate_negative_samples(self, _):
+        return np.random.choice(self._unseen_items, self._sample_size)
