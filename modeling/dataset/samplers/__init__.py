25c2ee7158cb3f2cbd433f830a7c00e244f275f3
Anton Dubrovin <dubrovinantonr@mail.ru>
2024-04-07 16:36:11 +0300

    fix error samplers init

--- modeling/dataset/samplers/__init__.py
+++ modeling/dataset/samplers/__init__.py
@@ -2,9 +2,14 @@ from .base import TrainSampler, ValidationSampler, EvalSampler
 from .base import MultiDomainTrainSampler, MultiDomainValidationSampler, MultiDomainEvalSampler
 from .cl4srec import Cl4SRecTrainSampler, Cl4SRecValidationSampler, Cl4SRecEvalSampler
 from .duorec import DuorecTrainSampler, DuoRecValidationSampler, DuoRecEvalSampler
-from .next_item_prediction import NextItemPredictionTrainSampler, NextItemPredictionValidationSampler, NextItemPredictionEvalSampler
-from .next_item_prediction import MultiDomainNextItemPredictionTrainSampler, MultiDomainNextItemPredictionValidationSampler, MultiDomainNextItemPredictionEvalSampler
-from .masked_item_prediction import MaskedItemPredictionTrainSampler, MaskedItemPredictionValidationSampler, MaskedItemPredictionEvalSampler
+from .next_item_prediction import NextItemPredictionTrainSampler, NextItemPredictionValidationSampler, \
+    NextItemPredictionEvalSampler
+from .next_item_prediction import MultiDomainNextItemPredictionTrainSampler, \
+    MultiDomainNextItemPredictionValidationSampler, MultiDomainNextItemPredictionEvalSampler
+from .next_item_prediction import NegativeRatingsTrainSampler, NegativeRatingsValidationSampler, \
+    NegativeRatingsEvalSampler
+from .masked_item_prediction import MaskedItemPredictionTrainSampler, MaskedItemPredictionValidationSampler, \
+    MaskedItemPredictionEvalSampler
 from .mclsr import MCLSRTrainSampler, MCLSRValidationSampler, MCLSRPredictionEvalSampler
 from .pop import PopTrainSampler, PopValidationSampler, PopEvalSampler
 from .s3rec import S3RecPretrainTrainSampler, S3RecPretrainValidationSampler, S3RecPretrainEvalSampler
