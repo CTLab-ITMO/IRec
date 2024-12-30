ddf015563449707f65e805d8cec02dcf2b6ba854
Noname Untitled <Noname-Untitled@yandex.ru>
2024-12-30 18:57:33 +0300

    Add time-based split for beauty dataset (#12)
    Add time-based split for beauty dataset

--- modeling/dataset/samplers/pop.py
+++ modeling/dataset/samplers/pop.py
@@ -2,7 +2,10 @@ from dataset.samplers.base import TrainSampler, EvalSampler
 
 import copy
 
+from collections import Counter
 
+
+# It's just a placeholder
 class PopTrainSampler(TrainSampler, config_name='pop'):
     pass
 
@@ -12,19 +15,15 @@ class PopEvalSampler(EvalSampler, config_name='pop'):
     def __init__(self, dataset, num_users, num_items):
         super().__init__(dataset, num_users, num_items)
 
-        self._item_2_count = {}
-        for i in range(1, num_items + 1):
-            self._item_2_count[i] = 0
-
+        self._item_2_count = Counter()
         for sample in dataset:
-            # items = sample['item.ids'][:-1]
             items = sample['item.ids']
             for item in items:
                 self._item_2_count[item] += 1
 
         self._candidates_counts = [0] + [
             self._item_2_count[item_id] for item_id in range(1, self._num_items + 1)
-        ]
+        ] + [0]  # Mask + padding
 
     @classmethod
     def create_from_config(cls, config, **kwargs):
