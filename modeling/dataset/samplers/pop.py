8cb8003ef48c829fd82e00a1616b3300538d9b52
Noname Untitled <Noname-Untitled@yandex.ru>
2024-12-30 18:50:55 +0300

    Fix pop statistics

--- modeling/dataset/samplers/pop.py
+++ modeling/dataset/samplers/pop.py
@@ -3,28 +3,46 @@ from dataset.samplers.base import TrainSampler, EvalSampler
 import copy
 
 
+CANDIDATE_COUNTS = None
+
+
 class PopTrainSampler(TrainSampler, config_name='pop'):
-    pass
+
+    def __init__(self, dataset, num_items):
+        super().__init__()
+        print('Run train pop sampler')
+
+        global CANDIDATE_COUNTS
+
+        if CANDIDATE_COUNTS is None:
+            item_2_count = {}
+            for i in range(1, num_items + 1):
+                item_2_count[i] = 0
+
+            for sample in dataset:
+                # items = sample['item.ids'][:-1]
+                items = sample['item.ids']
+                for item in items:
+                    item_2_count[item] += 1
+
+            CANDIDATE_COUNTS = [0] + [
+                item_2_count[item_id] for item_id in range(1, num_items + 1)
+            ] + [0]
+        
+    @classmethod
+    def create_from_config(cls, config, **kwargs):
+        return cls(
+            dataset=kwargs['dataset'],
+            num_items=kwargs['num_items']
+        )
 
 
 class PopEvalSampler(EvalSampler, config_name='pop'):
 
     def __init__(self, dataset, num_users, num_items):
         super().__init__(dataset, num_users, num_items)
-
-        self._item_2_count = {}
-        for i in range(1, num_items + 1):
-            self._item_2_count[i] = 0
-
-        for sample in dataset:
-            # items = sample['item.ids'][:-1]
-            items = sample['item.ids']
-            for item in items:
-                self._item_2_count[item] += 1
-
-        self._candidates_counts = [0] + [
-            self._item_2_count[item_id] for item_id in range(1, self._num_items + 1)
-        ] + [0]
+        print('Run eval pop sampler')
+    
 
     @classmethod
     def create_from_config(cls, config, **kwargs):
@@ -38,6 +56,9 @@ class PopEvalSampler(EvalSampler, config_name='pop'):
         sample = copy.deepcopy(self._dataset[index])
         labels = [sample['item.ids'][-1]]
 
+        global CANDIDATE_COUNTS
+        assert CANDIDATE_COUNTS is not None
+
         return {
             'user.ids': sample['user.ids'],
             'user.length': sample['user.length'],
@@ -45,6 +66,6 @@ class PopEvalSampler(EvalSampler, config_name='pop'):
             'labels.ids': labels,
             'labels.length': len(labels),
 
-            'candidates_counts.ids': self._candidates_counts,
-            'candidates_counts.length': len(self._candidates_counts)
+            'candidates_counts.ids': CANDIDATE_COUNTS,
+            'candidates_counts.length': len(CANDIDATE_COUNTS)
         }
