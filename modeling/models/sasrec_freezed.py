c25e49da3f89cbe7436cc9d293932f83905d7014
peterochek <peterkkor@gmail.com>
2025-02-14 10:16:15 +0300

    fix for sasrec weight only

--- modeling/models/sasrec_freezed.py
+++ modeling/models/sasrec_freezed.py
@@ -1,25 +1,23 @@
+import torch
 from models import SequentialTorchModel
 from utils import create_masked_tensor
 
-import torch
 
-
-class SasRecFreezedModel(SequentialTorchModel, config_name='sasrec_freezed'):
-
+class SasRecFreezedModel(SequentialTorchModel, config_name="sasrec_freezed"):
     def __init__(
-            self,
-            sequence_prefix,
-            positive_prefix,
-            num_items,
-            max_sequence_length,
-            embedding_dim,
-            num_heads,
-            num_layers,
-            dim_feedforward,
-            dropout=0.0,
-            activation='relu',
-            layer_norm_eps=1e-9,
-            initializer_range=0.02
+        self,
+        sequence_prefix,
+        positive_prefix,
+        num_items,
+        max_sequence_length,
+        embedding_dim,
+        num_heads,
+        num_layers,
+        dim_feedforward,
+        dropout=0.0,
+        activation="relu",
+        layer_norm_eps=1e-9,
+        initializer_range=0.02,
     ):
         super().__init__(
             num_items=num_items,
@@ -31,127 +29,136 @@ class SasRecFreezedModel(SequentialTorchModel, config_name='sasrec_freezed'):
             dropout=dropout,
             activation=activation,
             layer_norm_eps=layer_norm_eps,
-            is_causal=True
+            is_causal=True,
         )
         self._sequence_prefix = sequence_prefix
         self._positive_prefix = positive_prefix
 
         self._init_weights(initializer_range)
-        
-        df = torch.load('../data/Beauty/data_full.pt')
+
+        df = torch.load("../data/Beauty/data_full.pt", weights_only=False)
         precomputed_embeddings = torch.stack(df.sort_index().embeddings.tolist())
-        
-        self._projector = torch.nn.Linear(precomputed_embeddings.shape[1], embedding_dim)
-        
+
+        self._projector = torch.nn.Linear(
+            precomputed_embeddings.shape[1], embedding_dim
+        )
+
         padding_embedding = torch.nn.init.trunc_normal_(
             torch.zeros(1, precomputed_embeddings.shape[1]),
             std=initializer_range,
             a=-2 * initializer_range,
-            b=2 * initializer_range
+            b=2 * initializer_range,
         )
 
         mask_embedding = torch.nn.init.trunc_normal_(
             torch.zeros(1, precomputed_embeddings.shape[1]),
             std=initializer_range,
             a=-2 * initializer_range,
-            b=2 * initializer_range
+            b=2 * initializer_range,
         )
-        
-        extended_embeddings = torch.cat([padding_embedding, precomputed_embeddings, mask_embedding], dim=0)  # Shape: (num_items + 2, embedding_dim)
-        
+
+        extended_embeddings = torch.cat(
+            [padding_embedding, precomputed_embeddings, mask_embedding], dim=0
+        )  # Shape: (num_items + 2, embedding_dim)
+
         self._item_embeddings = torch.nn.Embedding(
-            num_embeddings=num_items + 2,
-            embedding_dim=precomputed_embeddings.shape[1]
+            num_embeddings=num_items + 2, embedding_dim=precomputed_embeddings.shape[1]
         )
-        
+
         # TODO ask about freezed masked & padding tokens
-        
+
         self._item_embeddings.weight.data.copy_(extended_embeddings)
         self._item_embeddings.weight.requires_grad = False
 
     @classmethod
     def create_from_config(cls, config, **kwargs):
         return cls(
-            sequence_prefix=config['sequence_prefix'],
-            positive_prefix=config['positive_prefix'],
-            num_items=kwargs['num_items'],
-            max_sequence_length=kwargs['max_sequence_length'],
-            embedding_dim=config['embedding_dim'],
-            num_heads=config.get('num_heads', int(config['embedding_dim'] // 64)),
-            num_layers=config['num_layers'],
-            dim_feedforward=config.get('dim_feedforward', 4 * config['embedding_dim']),
-            dropout=config.get('dropout', 0.0),
-            initializer_range=config.get('initializer_range', 0.02)
+            sequence_prefix=config["sequence_prefix"],
+            positive_prefix=config["positive_prefix"],
+            num_items=kwargs["num_items"],
+            max_sequence_length=kwargs["max_sequence_length"],
+            embedding_dim=config["embedding_dim"],
+            num_heads=config.get("num_heads", int(config["embedding_dim"] // 64)),
+            num_layers=config["num_layers"],
+            dim_feedforward=config.get("dim_feedforward", 4 * config["embedding_dim"]),
+            dropout=config.get("dropout", 0.0),
+            initializer_range=config.get("initializer_range", 0.02),
         )
-        
-    def get_item_embeddings(self, events): # TODO refactor (single projection)
+
+    def get_item_embeddings(self, events):  # TODO refactor (single projection)
         return self._projector(self._item_embeddings(events))
-    
+
     def get_last_item_embeddings(self):
         return self._projector(self._item_embeddings.weight)
 
     def forward(self, inputs):
-        all_sample_events = inputs['{}.ids'.format(self._sequence_prefix)]  # (all_batch_events)
-        all_sample_lengths = inputs['{}.length'.format(self._sequence_prefix)]  # (batch_size)
+        all_sample_events = inputs[
+            "{}.ids".format(self._sequence_prefix)
+        ]  # (all_batch_events)
+        all_sample_lengths = inputs[
+            "{}.length".format(self._sequence_prefix)
+        ]  # (batch_size)
 
         embeddings, mask = self._apply_sequential_encoder(
             all_sample_events, all_sample_lengths
         )  # (batch_size, seq_len, embedding_dim), (batch_size, seq_len)
 
         if self.training:  # training mode
-            all_positive_sample_events = inputs['{}.ids'.format(self._positive_prefix)]  # (all_batch_events)
+            all_positive_sample_events = inputs[
+                "{}.ids".format(self._positive_prefix)
+            ]  # (all_batch_events)
 
-            all_sample_embeddings = embeddings[mask]  # (all_batch_events, embedding_dim)
+            all_sample_embeddings = embeddings[
+                mask
+            ]  # (all_batch_events, embedding_dim)
 
-            all_embeddings = self.get_last_item_embeddings()  # (num_items + 2, embedding_dim)
+            all_embeddings = (
+                self.get_last_item_embeddings()
+            )  # (num_items + 2, embedding_dim)
 
             # a -- all_batch_events, n -- num_items + 2, d -- embedding_dim
             all_scores = torch.einsum(
-                'ad,nd->an',
-                all_sample_embeddings,
-                all_embeddings
+                "ad,nd->an", all_sample_embeddings, all_embeddings
             )  # (all_batch_events, num_items + 2)
 
             positive_scores = torch.gather(
-                input=all_scores,
-                dim=1,
-                index=all_positive_sample_events[..., None]
+                input=all_scores, dim=1, index=all_positive_sample_events[..., None]
             )  # (all_batch_items, 1)
 
             sample_ids, _ = create_masked_tensor(
-                data=all_sample_events,
-                lengths=all_sample_lengths
+                data=all_sample_events, lengths=all_sample_lengths
             )  # (batch_size, seq_len)
 
-            sample_ids = torch.repeat_interleave(sample_ids, all_sample_lengths, dim=0)  # (all_batch_events, seq_len)
+            sample_ids = torch.repeat_interleave(
+                sample_ids, all_sample_lengths, dim=0
+            )  # (all_batch_events, seq_len)
 
             negative_scores = torch.scatter(
                 input=all_scores,
                 dim=1,
                 index=sample_ids,
-                src=torch.ones_like(sample_ids) * (-torch.inf)
+                src=torch.ones_like(sample_ids) * (-torch.inf),
             )  # (all_batch_events, num_items + 2)
             negative_scores[:, 0] = -torch.inf  # Padding idx
-            negative_scores[:, self._num_items + 1:] = -torch.inf  # Mask idx
+            negative_scores[:, self._num_items + 1 :] = -torch.inf  # Mask idx
 
             return {
-                'positive_scores': positive_scores,
-                'negative_scores': negative_scores
+                "positive_scores": positive_scores,
+                "negative_scores": negative_scores,
             }
         else:  # eval mode
-            last_embeddings = self._get_last_embedding(embeddings, mask)  # (batch_size, embedding_dim)
+            last_embeddings = self._get_last_embedding(
+                embeddings, mask
+            )  # (batch_size, embedding_dim)
             # b - batch_size, n - num_candidates, d - embedding_dim
             candidate_scores = torch.einsum(
-                'bd,nd->bn',
-                last_embeddings,
-                self.get_last_item_embeddings()
+                "bd,nd->bn", last_embeddings, self.get_last_item_embeddings()
             )  # (batch_size, num_items + 2)
             candidate_scores[:, 0] = -torch.inf  # Padding id
-            candidate_scores[:, self._num_items + 1:] = -torch.inf  # Mask id
+            candidate_scores[:, self._num_items + 1 :] = -torch.inf  # Mask id
 
             _, indices = torch.topk(
-                candidate_scores,
-                k=20, dim=-1, largest=True
+                candidate_scores, k=20, dim=-1, largest=True
             )  # (batch_size, 20)
 
             return indices
