7bd0b2ef8edb8e9c4c3b80e417372ac41ddbb4e0
iskbaga <112892889+iskbaga@users.noreply.github.com>
2025-03-21 13:12:58 +0300

    попытка 2

--- modeling/models/tiger_from_sasrec.py
+++ modeling/models/tiger_from_sasrec.py
@@ -1,31 +1,33 @@
-import torch
-from models import SequentialTorchModel
-from torch import nn
-from utils import DEVICE, create_masked_tensor
+from collections import defaultdict
 
+import torch
+from torch import nn
+
+from models import SequentialTorchModel
+from utils import DEVICE, create_masked_tensor
 from .tiger import TigerModel
 
 
 class TigerFromSasRec(SequentialTorchModel, config_name="tiger_from_sasrec"):
     def __init__(
-        self,
-        rqvae_model,
-        item_id_to_semantic_id,
-        item_id_to_residual,
-        sequence_prefix,
-        positive_prefix,
-        negative_prefix,
-        num_items,
-        num_users,
-        max_sequence_length,
-        embedding_dim,
-        num_heads,
-        num_layers,
-        dim_feedforward,
-        dropout=0.0,
-        activation="relu",
-        layer_norm_eps=1e-9,
-        initializer_range=0.02,
+            self,
+            rqvae_model,
+            item_id_to_semantic_id,
+            item_ids,
+            sequence_prefix,
+            positive_prefix,
+            negative_prefix,
+            num_items,
+            num_users,
+            max_sequence_length,
+            embedding_dim,
+            num_heads,
+            num_layers,
+            dim_feedforward,
+            dropout=0.0,
+            activation="relu",
+            layer_norm_eps=1e-9,
+            initializer_range=0.02,
     ):
         super().__init__(
             num_items=num_items,
@@ -56,23 +58,73 @@ class TigerFromSasRec(SequentialTorchModel, config_name="tiger_from_sasrec"):
             num_embeddings=self._num_users + 1, embedding_dim=embedding_dim
         )
 
+        self.sem_id_len = 4
+        self._codebook_item_embeddings_stacked = nn.Parameter(
+            torch.stack([
+                *[codebook for codebook in rqvae_model.codebooks],
+                self.create_new_codebook(rqvae_model.codebooks[-1])
+            ]),
+            requires_grad=True
+        )
+
         self._init_weights(initializer_range)
 
-        self._item_id_to_semantic_embedding = nn.Parameter(
-            self.get_init_item_embeddings(
-                rqvae_model, item_id_to_semantic_id, item_id_to_residual
-            ),
-            requires_grad=True,
+        self._item_id_to_semantic_id = item_id_to_semantic_id
+        self.item_ids = item_ids
+
+    @staticmethod
+    def create_new_codebook(last_codebook, scale=1):
+
+        new_codebook = torch.randn_like(last_codebook.data)
+
+        norm_last = torch.norm(last_codebook, dim=1).mean()
+        norm_new = torch.norm(new_codebook, dim=1).mean()
+        scaling_factor = (norm_last / (scale * norm_new))
+
+        return new_codebook * scaling_factor
+
+    @staticmethod
+    def get_full_sids(sids, ids, codebook_size):
+        assert sids.shape[0] == ids.shape[0]
+
+        ids = ids.detach().to(DEVICE)
+        sids = sids.detach().to(DEVICE)
+
+        key = torch.tensor([codebook_size ** i for i in range(sids.shape[1])], device=DEVICE, requires_grad=False)
+
+        shuffled_indices = torch.randperm(len(ids), device=DEVICE)
+        shuffled_ids = ids[shuffled_indices]
+        shuffled_sids = sids[shuffled_indices]
+
+        col_tokens = torch.zeros(ids.shape, device=DEVICE, dtype=torch.long, requires_grad=False)
+
+        hash_dict = defaultdict(int)
+
+        for (i, sid) in enumerate(shuffled_sids):
+            sid_hash = (sid * key).sum().item()
+            col_tokens[i] = hash_dict[sid_hash]
+            hash_dict[sid_hash] += 1
+
+        full_sids = torch.cat([shuffled_sids, col_tokens.unsqueeze(1)], dim=1)
+        unshuffled_indices = shuffled_indices.argsort()
+
+        return (
+            shuffled_ids[unshuffled_indices].detach(),
+            full_sids[unshuffled_indices].detach()
         )
 
     @classmethod
     def create_from_config(cls, config, **kwargs):
-        rqvae_model, semantic_ids, residuals, _ = TigerModel.init_rqvae(config)
+        rqvae_model, sids, residuals, ids = TigerModel.init_rqvae(config)
+
+        ids_tensor = torch.tensor(ids, dtype=torch.long, device=DEVICE, requires_grad=False)
+        sids_tensor = sids.clone().detach()
+        item_ids, semantic_ids = cls.get_full_sids(sids_tensor, ids_tensor, rqvae_model.codebook_sizes[0])
 
         return cls(
             rqvae_model=rqvae_model,
             item_id_to_semantic_id=semantic_ids,
-            item_id_to_residual=residuals,
+            item_ids=item_ids,
             sequence_prefix=config["sequence_prefix"],
             positive_prefix=config["positive_prefix"],
             negative_prefix=config["negative_prefix"],
@@ -86,6 +138,10 @@ class TigerFromSasRec(SequentialTorchModel, config_name="tiger_from_sasrec"):
             dropout=config.get("dropout", 0.0),
             initializer_range=config.get("initializer_range", 0.02),
         )
+
+    def _item_id_to_semantic_embedding(self, events):
+        # TODO убрать sum потом
+        return self._get_embeddings(self._item_id_to_semantic_id[events - 1]).sum(dim=1).squeeze(1)
 
     def forward(self, inputs):
         all_sample_events = inputs[
@@ -107,7 +163,7 @@ class TigerFromSasRec(SequentialTorchModel, config_name="tiger_from_sasrec"):
             embeddings, mask
         )  # (batch_size, embedding_dim)
 
-        item_embeddings = self._item_id_to_semantic_embedding.sum(dim=1)
+        item_embeddings = self._item_id_to_semantic_embedding(self.item_ids)
 
         if self.training:  # training mode
             # positives
@@ -116,7 +172,7 @@ class TigerFromSasRec(SequentialTorchModel, config_name="tiger_from_sasrec"):
             ]  # (all_batch_events)
             in_batch_positive_embeddings = item_embeddings[
                 in_batch_positive_events - 1
-            ]  # (all_batch_events, embedding_dim)
+                ]  # (all_batch_events, embedding_dim)
             positive_scores = torch.einsum(
                 "bd,bd->b", last_embeddings, in_batch_positive_embeddings
             )  # (all_batch_events)
@@ -129,7 +185,7 @@ class TigerFromSasRec(SequentialTorchModel, config_name="tiger_from_sasrec"):
             ]  # (all_batch_events)
             in_batch_negative_embeddings = item_embeddings[
                 in_batch_negative_events - 1
-            ]  # (all_batch_events, embedding_dim)
+                ]  # (all_batch_events, embedding_dim)
             negative_scores = torch.einsum(
                 "bd,bd->b", last_embeddings, in_batch_negative_embeddings
             )  # (all_batch_events)
@@ -152,41 +208,45 @@ class TigerFromSasRec(SequentialTorchModel, config_name="tiger_from_sasrec"):
 
             return indices + 1  # tensors are 0 indexed
 
+    def _get_embeddings(self, sem_ids: torch.Tensor) -> torch.Tensor:
+        """
+        :param sem_ids: набор из sem ids (n, sem_id_len)
+        :return: эмбеддинг для каждого sem_id из набора (n, sem_id_len, embedding_dim)
+        """
+        assert sem_ids.shape[1] == self.sem_id_len
+        # [[1, 2, 3, 4], [5, 6, 7, 8]] TODO проверить еще раз
+
+        stacked_codebooks = self._codebook_item_embeddings_stacked.reshape(
+            self.sem_id_len * self._codebook_sizes[0], self._embedding_dim)
+
+        offsets = torch.tensor([0, 256, 512, 768], dtype=torch.long, device=DEVICE, requires_grad=False)
+        sem_ids_with_offsets = sem_ids + offsets.unsqueeze(0)
+
+        return stacked_codebooks[sem_ids_with_offsets]
+
+    def assert_item_range(self, events):
+        return torch.all((1 <= events) & (events <= self._item_id_to_semantic_id.shape[0])).item()
+
     def get_item_embeddings(self, events):
-        embs = self._item_id_to_semantic_embedding[
-            events - 1
-        ]  # len(events), len(self._codebook_sizes) + 1, embedding_dim
-        return embs.reshape(-1, self._embedding_dim)
-
-    def get_init_item_embeddings(
-        self, rqvae_model, item_id_to_semantic_id, item_id_to_residual
-    ):
-        codebooks = torch.stack([codebook for codebook in rqvae_model.codebooks])
-
-        result = []
-        for semantic_id in item_id_to_semantic_id:
-            item_repr = []
-            for codebook_idx, codebook_id in enumerate(semantic_id):
-                item_repr.append(codebooks[codebook_idx][codebook_id])
-            result.append(torch.stack(item_repr))
-
-        semantic_embeddings = torch.stack(
-            result
-        )  # len(events), len(codebook_sizes), embedding_dim
-
-        residual = item_id_to_residual.unsqueeze(1)
-
-        # get true item embeddings
-        item_embeddings = torch.cat(
-            [semantic_embeddings, residual], dim=1
-        )  # len(events), len(self._codebook_sizes) + 1, embedding_dim
-
-        return item_embeddings
+        assert len(events.shape) == 1
+        assert self.assert_item_range(events)
+        sids = self._item_id_to_semantic_id[events - 1]
+        assert sids.shape == (events.shape[0], self.sem_id_len)
+        sid_embs = self._get_embeddings(sids)
+        assert sid_embs.shape == (events.shape[0], self.sem_id_len, self._embedding_dim)
+        # # TODO убрать sum потом
+        # result = sid_embs.sum(dim=1).squeeze(1)
+        # assert result.shape ==  (events.shape[0], self._embedding_dim)
+        # return result
+        result = sid_embs.reshape(self.sem_id_len * events.shape[0],
+                                  self._embedding_dim)  # (self.sem_id_len * ..., embedding_dim)
+        assert result.shape == (self.sem_id_len * events.shape[0], self._embedding_dim)
+        return result
 
     def _encoder_pos_embeddings(self, lengths, mask):
         def position_lambda(x):
             return x // (
-                len(self._codebook_sizes) + 1
+                    len(self._codebook_sizes) + 1
             )  # 5 5 5 5 4 4 4 4 ..., +1 for residual
 
         position_embeddings = self._get_position_embeddings(
@@ -224,7 +284,7 @@ class TigerFromSasRec(SequentialTorchModel, config_name="tiger_from_sasrec"):
         # print(f"{positions.tolist()[:20]=}")
 
         assert (positions >= 0).all() and (
-            positions < embedding_layer.num_embeddings
+                positions < embedding_layer.num_embeddings
         ).all()
 
         position_embeddings = embedding_layer(
