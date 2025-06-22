5498b2bb1fce97814ab4c5514547e8ed14f0ab57
pavel krasnov <pat2709783@gmail.com>
2025-06-22 13:08:33 +0300

    handling new logic in samplers

--- src/irec/dataset/samplers/mclsr.py
+++ src/irec/dataset/samplers/mclsr.py
@@ -1,21 +1,26 @@
 from irec.dataset.samplers.base import TrainSampler, EvalSampler
 
 import copy
+import numpy as np
 
 
 class MCLSRTrainSampler(TrainSampler, config_name='mclsr'):
-    def __init__(self, dataset, num_users, num_items):
+    def __init__(self, dataset, num_users, num_items, num_negatives=100):
         super().__init__()
         self._dataset = dataset
         self._num_users = num_users
         self._num_items = num_items
+        self._num_negatives = num_negatives
+        self._all_items = np.arange(1, num_items + 1)
 
     @classmethod
     def create_from_config(cls, config, **kwargs):
+        num_negatives = config.get('num_negatives_train', 100)
         return cls(
             dataset=kwargs['dataset'],
             num_users=kwargs['num_users'],
             num_items=kwargs['num_items'],
+            num_negatives=num_negatives,
         )
 
     def __getitem__(self, index):
@@ -23,21 +28,35 @@ class MCLSRTrainSampler(TrainSampler, config_name='mclsr'):
 
         item_sequence = sample['item.ids'][:-1]
         next_item = sample['item.ids'][-1]
-        next_item_sequence = sample['item.ids'][1:]
+
+        seen_items = set(item_sequence + [next_item])
+        
+        negatives = []
+        while len(negatives) < self._num_negatives:
+            candidates = np.random.choice(self._all_items, size=self._num_negatives * 2) 
+            
+            for item_id in candidates:
+                if item_id not in seen_items:
+                    negatives.append(item_id)
+                    if len(negatives) == self._num_negatives:
+                        break
 
         return {
             'user.ids': sample['user.ids'],
             'user.length': sample['user.length'],
             'item.ids': item_sequence,
             'item.length': len(item_sequence),
-            'positive.ids': next_item_sequence,
-            'positive.length': len(next_item_sequence),
             'labels.ids': [next_item],
             'labels.length': 1,
+            'negatives.ids': negatives,
+            'negatives.length': len(negatives),
         }
 
 
 class MCLSRPredictionEvalSampler(EvalSampler, config_name='mclsr'):
+    def __init__(self, dataset, num_users, num_items):
+        super().__init__(dataset, num_users, num_items)
+
     @classmethod
     def create_from_config(cls, config, **kwargs):
         return cls(
@@ -45,3 +64,17 @@ class MCLSRPredictionEvalSampler(EvalSampler, config_name='mclsr'):
             num_users=kwargs['num_users'],
             num_items=kwargs['num_items'],
         )
+    
+    def __getitem__(self, index):
+        sample = self._dataset[index]
+        history_sequence = sample['history']
+        target_items = sample['target']
+
+        return {
+            'user.ids': sample['user.ids'],
+            'user.length': 1,
+            'item.ids': history_sequence,
+            'item.length': len(history_sequence),
+            'labels.ids': target_items,
+            'labels.length': len(target_items),
+        }
